dfd8801ef3b85f9f15f0ef93b6216578f8338000
soumyaathanimath <>
2024-05-16 15:00:10 +0530

    Sorting of array

---
+++ src/main/java/com/mycompany/airthmatic/Airthmatic.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airthmatic;
+
+/**
+ *
+ * @author dell1
+ */
+public class Airthmatic {
+
+    public static void main(String[] args)
+    {
+        int a=14,b=15;
+                int c=a/b;
+                        int r=a%b;
+                                System.out.println(c);
+        System.out.println(r);
+        
+    }
+}
